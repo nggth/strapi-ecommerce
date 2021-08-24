88dc9a9f051e7b980c0139f083eb087f3ef46b55
ngthh <saccarozo.29@gmail.com>
2021-08-24 14:50:37 +0700

    _app.js

--- ecommerce-frontend/pages/_app.js
+++ ecommerce-frontend/pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Header from '../components/Header'
+import { AuthProvider } from '../context/AuthContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <content>
+        <Header />
+        <Component {...pageProps} />
+      </content>
+    </AuthProvider>
+  )
 }
 
 export default MyApp
