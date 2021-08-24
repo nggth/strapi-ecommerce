9833f947dccfc56c5538b4b3254ee418f5e7ff85
ngthh <saccarozo.29@gmail.com>
2021-08-24 14:49:42 +0700

    Home

--- ecommerce-frontend/pages/index.js
+++ ecommerce-frontend/pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
-import products from '../products.json'
-import { fromImageToUrl } from '../utils/urls'
+import { fromImageToUrl, API_URL } from '../utils/urls'
+import { twoDecimals } from '../utils/format'
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <div>
       <Head>
@@ -15,25 +15,35 @@ export default function Home() {
 
       {
         products.map(product => (
-          <div>
             <div key={product.name} className={styles.product}>
-              <Link href={`/products/${product.slug}`}>
-                <a>
-                  <div className={styles.product_Row}>
-                    <div className={styles.product_ColImg}>
-                      <img src={fromImageToUrl(product.image)} />
+              <a>
+                <Link href={`/products/${product.slug}`}>
+                    <div className={styles.product__Row}>
+                      <div className={styles.product__ColImg}>
+                        <img src={fromImageToUrl(product.image)} />
+                      </div>
+                      <div className={styles.product__Col}>
+                        {product.name} ${twoDecimals(product.price)}
+                      </div>
                     </div>
-                    <div className={styles.product_ColImg}>
-                      {product.name} {product.price}
-                    </div>
-                  </div>
-                </a>
-              </Link>
-            </div>
-
+                </Link>
+               </a>
           </div>
         ))
       }
     </div>
   )
+}
+
+export async function getStaticProps() {
+  //Fetch product
+  const product_res = await fetch(`${API_URL}/products/`)
+  const products = await product_res.json()
+
+  //Return the products as props
+  return {
+    props: {
+      products
+    }
+  }
 }
